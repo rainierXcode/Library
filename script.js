713562b191d37f154ffefda80f785ace9558690b
Rainier Sapin <rainiersapin0131@gmail.com>
2023-09-07 11:41:13 +0800

    Delete Item Funcationality

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const toggleButton = document.getElementById("toggleButton");
+const addBook = document.getElementById("add-book-button");
+const form = document.querySelector('form');
+const formClose = document.getElementById("close-icon-button");
+const formContainer = document.querySelector(".form-container");
+const cards = document.querySelectorAll('.card');
 
-toggleButton.addEventListener("click", () => {
-    toggleButton.classList.toggle("active");
+cards.forEach((card) => {
+    const deleteButton = card.querySelector('.delete-item-button');
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    });
+});
+
+addBook.addEventListener('click', () =>{
+    form.reset();
+    formContainer.style.display = 'block';
+    formContainer.classList.remove('zoom-out');
+    formContainer.classList.add('zoom-in');
+});
+
+formClose.addEventListener('click', () =>{
+    formContainer.classList.remove('zoom-in');
+    formContainer.classList.add('zoom-out');
+    setTimeout(() => {
+        formContainer.style.display = 'none';
+    }, 300); 
 });
